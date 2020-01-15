c68cad0d3705e218d33f9918f789d463ca80e29d
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-01-15 15:59:51 +0100

    support for hard float Cortex-A cores

--- assemble.sh
+++ assemble.sh
@@ -28,10 +28,11 @@ ar crs bin/thumbv8m.main-none-eabi.a bin/$crate.o
 ar crs bin/thumbv8m.main-none-eabihf.a bin/$crate.o
 
 # Cortex-A*
-# NOTE: cflags taken from cc 1.0.49 / armv7-unknown-linux-gnueabi
+# NOTE: cflags taken from cc 1.0.49 / armv7-unknown-linux-gnueabi{,hf}
 arm-none-eabi-as -march=armv7-a asm-arm.s -o bin/$crate.o
 ar crs bin/armv7-none-eabi.a bin/$crate.o
 
-# TODO `armv7-none-eabiHF`
+arm-none-eabi-as -march=armv7-a+vfpv3 asm-arm.s -o bin/$crate.o
+ar crs bin/armv7-none-eabihf.a bin/$crate.o
 
 rm bin/$crate.o
